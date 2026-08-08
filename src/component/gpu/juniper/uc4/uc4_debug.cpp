f5b7e2716083688ef83b5fa5a272d635bf71b09f
starfrost013 <mario64crashed@gmail.com>
2026-08-08 19:13:20 +0100

    add explanations for what are uc4 Buffers

--- src/component/gpu/juniper/uc4/uc4_debug.cpp
+++ src/component/gpu/juniper/uc4/uc4_debug.cpp
@@ -31,16 +31,17 @@ namespace Motion
             ImGui::Text("Busy: %s", (uc4->ucr & UC4_UCR_BUSY) ? "True" : "False");
 
             ImGui::TextColored(CoherentUI::COLOUR_HEADER, "Buffers");
-            ImGui::Text("EDB: 0x%x ECB: 0x%x", uc4->edb, uc4->ecb);
-            ImGui::Text("X start: %d X End: %d", uc4->xsb, uc4->xeb);
-            ImGui::Text("Y start: %d Y End: %d", uc4->ysb, uc4->yeb);
-            ImGui::Text("FMAB: 0x%x", uc4->fmab);
-            ImGui::Text("DDASAF: 0x%x DDASAI: 0x%x", uc4->ddasaf, uc4->ddasai);
-            ImGui::Text("DDAEAF: 0x%x DDAEAI: 0x%x", uc4->ddaeaf, uc4->ddaeai);
-            ImGui::Text("DDASDF: 0x%x DDASDI: 0x%x", uc4->ddasdf, uc4->ddasdi);
-            ImGui::Text("DDAEDF: 0x%x DDAEDI: 0x%x", uc4->ddaedf, uc4->ddaedi);
+            ImGui::Text("Bresenham Error Delta (EDB): 0x%x Bresenham Error Correction (ECB): 0x%x", uc4->edb, uc4->ecb);
+            ImGui::Text("X start (XSB): %d X end (XEB): %d", uc4->xsb, uc4->xeb);
+            ImGui::Text("Y start (YSB): %d Y end (YEB): %d", uc4->ysb, uc4->yeb);
+            ImGui::Text("Font Memory Address (FMAB): 0x%x", uc4->fmab);
+
+            ImGui::Text("DDA Start Address F: 0x%x DDA Start Address I: 0x%x", uc4->ddasaf, uc4->ddasai);
+            ImGui::Text("DDA End Address F: 0x%x DDA End Address I: 0x%x", uc4->ddaeaf, uc4->ddaeai);
+            ImGui::Text("DDA Start Delta High (F): 0x%x DDA Start Delta Low (I): 0x%x", uc4->ddasdf, uc4->ddasdi);
+            ImGui::Text("DDA End Delta High (F): 0x%x DDA End Delta Low (I): 0x%x", uc4->ddaedf, uc4->ddaedi);
             ImGui::Text("Mode: 0x%x", uc4->mode);
-            ImGui::Text("Repeat: 0x%x", uc4->repeat);
+            ImGui::Text("Line Stipple Repeat (RPB): 0x%x", uc4->repeat);
             ImGui::Text("Config: 0x%x", uc4->config);
 
             ImGui::TextColored(CoherentUI::COLOUR_HEADER, "Commands");
