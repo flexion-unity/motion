8f10663a1282e5d5470c83ba14e4b5e9b12314f4
starfrost013 <mario64crashed@gmail.com>
2026-09-02 01:18:35 +0100

    [skip ci] increment coherent version for 0.3

--- src/coherent/coherent.hpp
+++ src/coherent/coherent.hpp
@@ -16,7 +16,7 @@
 namespace Motion
 {
     #define COHERENT_LOG_PREFIX     "Debugger"
-    #define COHERENT_VERSION        "Coherent v0.8 (August 16, 2026)"
+    #define COHERENT_VERSION        "Coherent v0.9 (September 1, 2026)"
 
     extern Cvar* startPaused;
 
